a0bf9edb1961592f756f9a71347494522a17d78d
Feddman <feddevangils@gmail.com>
2015-02-19 17:21:22 +0100

    add player object and attached inventory to player;

--- Program.cs
+++ Program.cs
@@ -4,28 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace inventory
+namespace rpg
 {
     class Program
     {
         static void Main(string[] args)
-        {
-            Inventory inventory = new Inventory(200);
+        {   
+            Player fedde = new Player("fedde");
 
             Item apple = new Item("apple", 30);
             Item sword = new Item("sword", 100);
 
             Item dragonShield = new Item("Dragon Shield", 400);
 
-            inventory.addItem(apple);
-            inventory.addItem(apple);
-            inventory.addItem(sword);
 
 
-            inventory.viewItems();
+            fedde.inventory.addItem(apple);
+            fedde.inventory.addItem(apple);
+            fedde.inventory.addItem(sword);
 
 
-            inventory.addItem(dragonShield); 
+            fedde.inventory.viewItems();
+            
+
+            fedde.inventory.addItem(dragonShield); 
 
 
         }
---
+++ player/Player.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rpg
+{
+    class Player
+    {
+        private string name;
+       
+        private int strength;
+        private int magic;
+        private int dex;
+
+        private int level;
+
+        private int health;
+        private int damage; // how many damage absorbed per hit
+        
+        private int mana;
+
+
+        public Inventory inventory;
+
+        public Player(string name)
+        {
+            this.name = name;
+            this.inventory = new Inventory(250);
+        }
+
+        public void attack()
+        {
+            Console.WriteLine(this.name + " attacks");
+        }
+
+        public void escape()
+        {
+            Console.WriteLine(this.name + " escapes!");
+        }
+
+        public void usePotion(Item item)
+        {
+            Console.WriteLine(this.name + "used " + item);
+        }
+        
+    }
+}
